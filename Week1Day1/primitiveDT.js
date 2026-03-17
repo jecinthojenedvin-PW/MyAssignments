413cf80697f9cf885ca872660d0077baf7f4d9ff
Jenett <jecinthojenedvin@gmail.com>
2026-03-17 18:11:59 -0400

    Add files via upload

---
+++ Week1Day1/primitiveDT.js
@@ -0,0 +1,35 @@
+
+//string
+
+let username = 'Testleaf'
+console.log('Username:',username);
+console.log('DataType:', typeof username);
+
+//Number
+
+let associateCount = 150
+console.log('Associate Count:',associateCount);
+console.log('DataType:', typeof associateCount);
+
+//Boolean
+
+let isAutomated = true
+console.log('Whether fully automated process (T/F):',isAutomated);
+console.log('DataType:', typeof isAutomated);
+
+//Undefined
+
+let address
+console.log('Address:',address);
+console.log('DataType:', typeof address);
+
+//bigint
+
+let companyId = 899999911112344566434230n
+console.log('CompanyID:',companyId);
+console.log('DataType:', typeof companyId);
+
+
+
+
+
